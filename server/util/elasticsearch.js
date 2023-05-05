a08a851d79f1ae2bd0e2cc35be409e5cc57715c1
Marvy Calderín <mcalderin@desidedatum.com>
2023-05-05 11:09:13 +0200

    Revert "agregar opcion ga4 para createPipeline + "

--- server/util/elasticsearch.js
+++ server/util/elasticsearch.js
@@ -5,7 +5,7 @@ const xml = require('xml2js');
 
 const client = new Client({ node: constants.ANALYTICS_ELASTIC_URL })
 var gapiUrl = 'http://localhost:50053';
-var ga4piUrl = 'http://localhost:50054';
+
 module.exports = {
     deletePortal: function (id) {
         client.indices.delete({
@@ -29,7 +29,7 @@ module.exports = {
             console.log(error);
         })
     },
-    reloadPortal: async function (portal, reloadDate, delay) {
+    reloadPortal: async function (portal, reloadDate) {
         await delete_date(reloadDate, portal.id_logstash)
         if (portal.type == 'urchin') {
             await browsers_urchin(portal, reloadDate);
@@ -43,13 +43,6 @@ module.exports = {
             await files_ga(portal, reloadDate);
             await countries_ga(portal, reloadDate);
         }
-        if (portal.type == 'ga4') {
-            await browsers_ga4(portal, delay);
-            await pages_ga4(portal, delay);
-            await files_ga4(portal, delay);
-            await countries_ga4(portal, delay);
-        }
-
     }
 };
 
@@ -329,120 +322,8 @@ async function browsers_ga(portal, date) {
     return index('logstash-reports-browsers-' + portal.id_logstash, responsevalue);
 }
 
-async function browsers_ga4(portal, delay) {
-    var requestUrl = ga4piUrl + '/browsers_ga?delay=' + delay + '&view=' + portal.view;
-
-    var http_promise = new Promise((resolve, reject) => {
-        http.get(requestUrl, (resp) => {
-            let data = '';
-            resp.on('data', (chunk) => {
-                data += chunk;
-            });
-            resp.on('end', () => {
-                try {
-                    var response = JSON.parse(data);
-                } catch (error) {
-                    resolve([]);
-                }
-                var indexdate = new Date(resp.headers['custom-date']);
-                var r = [];
-                if (response.reports && response.reports.length > 0) {
-                    response.reports[0].report.forEach(element => {
-                        var browser = element.browser;
-                        if (browser === '(not set)' || browser === '(unknown)') {
-                            browser = 'Desconocido';
-                        }
-                        var deviceCategory = element.deviceCategory;
-                        if (deviceCategory === '(not set)' || deviceCategory === '(unknown)') {
-                            deviceCategory = 'Desconocido';
-                        }
-                        var operatingSystem = element.operatingSystem;
-                        if (operatingSystem === '(not set)' || operatingSystem === '(unknown)') {
-                            operatingSystem = 'Desconocido';
-                        }
-                        var value = {
-                            "device": deviceCategory,
-                            "visits": parseInt(element.sessions),
-                            "browser_name": browser,
-                            "@timestamp": indexdate,
-                            "portal": portal.url,
-                            "platform_name": operatingSystem,
-                            "type": "browsers",
-                            "view": portal.view
-                        };
-                        r.push(value);
-                    });
-                }
-                resolve(r);
-            });
-            resp.on('error', (error) => {
-                reject(error);
-            });
-        })
-    });
-
-    let responsevalue = await http_promise;
-
-    return index('logstash-reports-browsers-' + portal.id_logstash, responsevalue);
-}
-
 async function pages_ga(portal, date) {
     var requestUrl = gapiUrl + '/pages_ga?view=' + portal.view + '&date=' + date.getTime() / 1000;
-    var http_promise = new Promise((resolve, reject) => {
-        http.get(requestUrl, (resp) => {
-            let data = '';
-            resp.on('data', (chunk) => {
-                data += chunk;
-            });
-            resp.on('end', async () => {
-                try {
-                    var response = JSON.parse(data);
-                } catch (error) {
-                    resolve([]);
-                }
-                var indexdate = new Date(resp.headers['custom-date']);
-                var r = [];
-                if (response.reports && response.reports.length > 0) {
-                    response.reports[0].report.forEach(element => {
-                        var hostname = element.hostname;
-                        if (hostname === '(not set)' || hostname === '(unknown)') {
-                            hostname = 'Desconocido';
-                        }
-                        var pagePath = element.pagePath;
-                        if (pagePath === '(not set)' || pagePath === '(unknown)') {
-                            pagePath = 'Desconocido';
-                        }
-                        var pageTitle = element.pageTitle;
-                        if (pageTitle === '(not set)' || pageTitle === '(unknown)') {
-                            pageTitle = 'Desconocido';
-                        }
-                        var value = {
-                            "visits": parseInt(element.sessions),
-                            "@timestamp": indexdate,
-                            "path": pagePath,
-                            "title": pageTitle,
-                            "portal": portal.url,
-                            "type": "pages",
-                            "view": portal.view
-                        };
-                        r.push(value);
-                    });
-                }
-                resolve(r);
-            });
-            resp.on('error', (error) => {
-                reject(error);
-            });
-        });
-    });
-
-    let responsevalue = await http_promise;
-
-    return index('logstash-reports-pages-' + portal.id_logstash, responsevalue);
-}
-
-async function pages_ga4(portal, delay) {
-    var requestUrl = ga4piUrl + '/pages_ga?delay=' + delay + '&view=' + portal.view;
     var http_promise = new Promise((resolve, reject) => {
         http.get(requestUrl, (resp) => {
             let data = '';
@@ -547,121 +428,8 @@ async function files_ga(portal, date) {
     return index('logstash-reports-files-' + portal.id_logstash, responsevalue);
 }
 
-async function files_ga4(portal, delay) {
-    var requestUrl = ga4piUrl + '/files_ga?delay=' + delay + '&view=' + portal.view;
-    var http_promise = new Promise((resolve, reject) => {
-        http.get(requestUrl, (resp) => {
-            let data = '';
-            resp.on('data', (chunk) => {
-                data += chunk;
-            });
-            resp.on('end', () => {
-                try {
-                    var response = JSON.parse(data);
-                } catch (error) {
-                    resolve([]);
-                }
-                var indexdate = new Date(resp.headers['custom-date']);
-                var r = [];
-                if (response.reports && response.reports.length > 0) {
-                    response.reports[0].report.forEach(element => {
-                        var eventAction = element.eventAction;
-                        if (eventAction === '(not set)' || eventAction === '(unknown)') {
-                            eventAction = 'Desconocido';
-                        }
-                        var eventLabel = element.eventLabel;
-                        if (eventLabel === '(not set)' || eventLabel === '(unknown)') {
-                            eventLabel = 'Desconocido';
-                        }
-                        var value = {
-                            "extension": eventAction,
-                            "@timestamp": indexdate,
-                            "path": eventLabel,
-                            "downloads": parseInt(element.totalEvents),
-                            "portal": portal.url,
-                            "type": "files",
-                            "view": portal.view
-                        };
-                        r.push(value);
-                    });
-                }
-                resolve(r);
-            });
-            resp.on('error', (error) => {
-                reject(error);
-            });
-        })
-    });
-
-    let responsevalue = await http_promise;
-
-    return index('logstash-reports-files-' + portal.id_logstash, responsevalue);
-}
-
 async function countries_ga(portal, date) {
     var requestUrl = gapiUrl + '/countries_ga?view=' + portal.view + '&date=' + date.getTime() / 1000;
-    var http_promise = new Promise((resolve, reject) => {
-        http.get(requestUrl, (resp) => {
-            let data = '';
-            resp.on('data', (chunk) => {
-                data += chunk;
-            });
-            resp.on('end', () => {
-                try {
-                    var response = JSON.parse(data);
-                } catch (error) {
-                    resolve([]);
-                }
-                var indexdate = new Date(resp.headers['custom-date']);
-                var r = [];
-                if (response.reports && response.reports.length > 0) {
-                    response.reports[0].report.forEach(element => {
-                        var city = element.city;
-                        if (city === '(not set)' || city === '(unknown)') {
-                            city = 'Desconocido';
-                        }
-                        var country = element.country;
-                        if (country === '(not set)' || country === '(unknown)') {
-                            country = 'Desconocido';
-                        }
-
-                        var region = element.region;
-                        if (region === '(not set)' || region === '(unknown)') {
-                            region = 'Desconocido';
-                        }
-                        var value = {
-                            "visits": parseInt(element.sessions),
-                            "geoip": {
-                                "location": {
-                                    "lon": parseFloat(element.longitude),
-                                    "lat": parseFloat(element.latitude)
-                                }
-                            },
-                            "city": city,
-                            "region": region,
-                            "@timestamp": indexdate,
-                            "portal": portal.url,
-                            "country": country,
-                            "type": "countries"
-                        };
-                        r.push(value);
-                    });
-                }
-                resolve(r);
-            });
-            resp.on('error', (error) => {
-                reject(error);
-            });
-        })
-    })
-
-    let responsevalue = await http_promise;
-
-    return index('logstash-reports-countries-' + portal.id_logstash, responsevalue);
-}
-
-async function countries_ga4(portal, delay) {
-    var requestUrl = ga4piUrl + '/countries_ga?delay=' + delay + '&view=' + portal.view;
     var http_promise = new Promise((resolve, reject) => {
         http.get(requestUrl, (resp) => {
             let data = '';
