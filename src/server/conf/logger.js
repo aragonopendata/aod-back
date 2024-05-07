719bfd7159ca67355e42ef74f59f58ae3e6e2388
Oriol Garcia <62403724+oriolgaal@users.noreply.github.com>
2024-05-07 17:55:15 +0200

    Added env variables for log path

--- src/server/conf/logger.js
+++ src/server/conf/logger.js
@@ -2,6 +2,4 @@ const logConfig = require('./log-conf');
 const loggerSettings = logConfig.getLogSettings();
 const logger = require('js-logging').dailyFile(loggerSettings);
 
-logger.warning('This is a warning message');
-
 module.exports = logger
